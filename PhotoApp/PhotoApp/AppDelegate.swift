8b5cef899de6498dcf33aa0ff28fcebe387396a3
1Consumption <hanseop95@gmail.com>
2021-01-07 12:22:14 +0900

    chore: 프로젝트 초기화
    - SceneDelegate는 iOS 12 버전에서 지원하지 않음
    - 또한 iPhone에서는 스플릿뷰를 지원하지 않기 때문에 삭제

---
+++ PhotoApp/PhotoApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PhotoApp
+//
+//  Created by 신한섭 on 2021/01/07.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by 신한섭 on 2021/01/07.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
