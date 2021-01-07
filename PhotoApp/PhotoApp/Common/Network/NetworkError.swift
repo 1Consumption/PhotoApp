cbb6e386dc53ac0b4e77ea907e0830a2367c83e5
1Consumption <hanseop95@gmail.com>
2021-01-07 18:42:01 +0900

    feat: NetworkManageable 프로토콜 생성
    - 네트워킹을 하기 위한 조건을 가지고있음
    - Network 에러를 나타내는 NetowrkError enum 생성
    - 중복되는 요청을 방지하기 위해 요청을 저장하는 set을 포함함.
    - URLSessionDataTask를 반환하는 메소드

---
+++ PhotoApp/PhotoApp/Common/Network/NetworkError.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkError.swift
+//  PhotoApp
+//
+//  Created by 신한섭 on 2021/01/07.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+}
---
+++ PhotoApp/PhotoApp/Common/Network/NetworkManageable.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManageable.swift
+//  PhotoApp
+//
+//  Created by 신한섭 on 2021/01/07.
+//
+
+import Foundation
+
+typealias DataResultHandler = (Result<Data, NetworkError>) -> Void
+
+protocol NetworkManageable: class {
+    var requestBag: Set<URL> { get }
+    
+    @discardableResult
+    func requestData(from url: String, method: HTTPMethod, completionHandler: @escaping DataResultHandler) -> URLSessionDataTask?
+    func requestCompleted()
+}
