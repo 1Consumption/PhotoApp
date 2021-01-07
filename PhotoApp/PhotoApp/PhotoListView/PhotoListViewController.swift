6b029503a06dce7ab3f8dfb8756d5b6f482d3f9d
1Consumption <hanseop95@gmail.com>
2021-01-07 18:02:12 +0900

    feat: UICollectionViewDelegate 채택
    - 하나의 line에 하나의 cell만 보여줌.
    - cell의 크기는 Image의 크기에 따라 가변적으로 변함. Image의 비율은 유지하면서, 넓이는 디바이스의 최대 넓이로 설정했음.

--- PhotoApp/PhotoApp/PhotoListView/PhotoListViewController.swift
+++ PhotoApp/PhotoApp/PhotoListView/PhotoListViewController.swift
@@ -7,26 +7,43 @@
 
 import UIKit
 
-final class PhotoListViewController: UIViewController {
+struct Photo {
+    let name: String
+    let author: String
+}
 
+final class PhotoListViewController: UIViewController {
+    
+    private var model: [Photo] = [Photo]()
     @IBOutlet weak var photoListCollectionView: UICollectionView!
     
     override func viewDidLoad() {
+        (1..<20).forEach { _ in
+            let number = Int.random(in: 1...8)
+            model.append(Photo(name: "sample\(number)", author: "Author \(number)"))
+        }
         super.viewDidLoad()
         photoListCollectionView.dataSource = self
+        photoListCollectionView.delegate = self
     }
 }
 
 extension PhotoListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return model.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoListCollectionViewCell.identifier, for: indexPath) as? PhotoListCollectionViewCell else { return UICollectionViewCell() }
-        let number = Int.random(in: 1...8)
-        cell.photoImageView.image = UIImage(named: "sample\(number)")
-        cell.authorNameLabel.text = "Author \(number)"
+        cell.photoImageView.image = UIImage(named: model[indexPath.item].name)
+        cell.authorNameLabel.text = "\(model[indexPath.item].author)"
         return cell
+    }
+}
+
+extension PhotoListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let size = UIImage(named: model[indexPath.item].name)?.size else { return .zero }
+        return CGSize(width: view.frame.width, height: size.height * view.frame.width / size.width)
     }
 }
