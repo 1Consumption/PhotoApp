47020319d51fb8aba8a368f370662a108051bdf7
1Consumption <hanseop95@gmail.com>
2021-01-11 00:56:27 +0900

    refactor: 한번에 하나의 동작만 바인딩 가능하던 Observable을 여러개의 동작과 바인딩 가능하도록 변경

--- PhotoApp/PhotoAppTests/Common/ObservableTests.swift
+++ PhotoApp/PhotoAppTests/Common/ObservableTests.swift
@@ -10,13 +10,22 @@ import XCTest
 
 final class ObservableTests: XCTestCase {
     func testBind() {
+        var bag = CancellableBag()
         let expectation = XCTestExpectation(description: "observable bind")
+        expectation.expectedFulfillmentCount = 2
+        
         let number = 5
         let observable = Observable<Int>()
+        
         observable.bind {
             XCTAssertEqual(number, $0)
             expectation.fulfill()
-        }
+        }.store(in: &bag)
+        
+        observable.bind {
+            XCTAssertEqual(number, $0)
+            expectation.fulfill()
+        }.store(in: &bag)
         
         observable.value = number
         
